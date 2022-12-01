7bc54977e5ab54ffacc824332c6506bf2f97de43
tomp736 <12602834+tomp736@users.noreply.github.com>
2022-12-01 20:48:06 +0100

    cleanup vars

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "hcloud_token" {
-  description = "(Required) - The Hetzner Cloud API Token, can also be specified with the HCLOUD_TOKEN environment variable."
-  type        = string
-  sensitive   = true
-}
-
 variable "public_key" {
   description = "(Required) - Public key for testing."
   type        = string
